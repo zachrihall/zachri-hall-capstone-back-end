b779019346a19beaaf9e53c1ce1827cc3ece4c3d
zachrihall <hallzachri@gmail.com>
2022-08-06 18:57:12 -0400

    merged socket.io and routes to one port

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const usersRoutes = require("./routes/usersRoutes");
 const postsRoutes = require("./routes/postsRoutes");
+const http = require("http");
+const { Server } = require('socket.io'); 
 
 
 app.use(cors());
@@ -13,17 +15,30 @@ app.use(express.json());
 app.use("/users", usersRoutes);
 app.use("/posts", postsRoutes);
 
+const server = http.createServer(app);
 
-
-// all warehouses routes
-app.get("/", (_req, res) => {
-  res.send("hi");
-})
-
-app.post("/", (req, _res, next) => {
-  const data = req.body;
-})
-
-app.listen(PORT, () => {
-  console.log(`Listening on port${PORT}`);
+const io = new Server(server, {
+  cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+  }
 });
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+      // socket.broadcast.emit("receive_message", data);
+      socket.to(data.room).emit("receive_message", data);
+  })
+
+  socket.on("join_room", (data) => {
+      socket.join(data);
+  })
+
+})
+
+
+server.listen(8080, () => {
+  console.log("SERVER RUNNIG ON PORT 8080");
+})
